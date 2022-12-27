f9482f67bfebef7b209e1d9990cad0e1546a6152
Sami <52921619+sbagh@users.noreply.github.com>
2022-12-27 17:10:57 -0500

    small fix

--- index.js
+++ index.js
@@ -10,29 +10,25 @@ chrome.storage.sync.set(link1);
 //    console.log(tab.id);
 // });
 
-async function currentTabID() {
+async function currentURL() {
    const windowId = (await chrome.windows.getCurrent()).id;
    const [tab] = await chrome.tabs.query({ active: true, windowId });
    if (tab) {
-      return tab.id;
+      return new URL(tab.url).hostname;
    } else {
       return "No tab found";
    }
 }
-
-chrome.tabs.onUpdated.addListener(function (current, changeInfo, tab) {
-   alert(changeInfo.url);
-});
 
 // new URL(tab.url).hostname;
 
 // 3- check if current host name is part of storage
 // if so return the countdown number (which is the correspdonding value of local storage key)
 
-// let countdownValue = currentURL().then((url) => {
-//    return chrome.storage.sync.get([url]).then((storageObject) => {
-//       return storageObject[url];
-//    });
-// });
+let countdownValue = currentURL().then((url) => {
+   return chrome.storage.sync.get([url]).then((storageObject) => {
+      return storageObject[url];
+   });
+});
 
 // 4-trigger countdown
